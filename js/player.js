4eeda74555a1028183fe4daeb8616b7049dd4094
theodore-404 <tioonlygame@gmail.com>
2026-07-07 16:36:04 +0700

    Update player.js

---
+++ js/player.js
@@ -0,0 +1,11 @@
+let playerName = "Captain Price";
+let health = 100;
+const inventory = [];
+
+let hasKeycard = false;
+let hasMedkit = false;
+let hasBandage = false;
+let hasPistol = false;
+let hasGeneratorKey = false;
+
+let zombieAlive = true;
